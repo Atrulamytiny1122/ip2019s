cb0a5db6bf1ea737e7c75bebc80f1444e47f8a51
Atrulamytiny1122 <52518066+Atrulamytiny1122@users.noreply.github.com>
2019-07-05 18:01:41 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,42 @@
 
 
 function start(e) {
+    var name = [
+        {
+            char:'曾',
+            big5:'B4BF',
+            cns:'1-5F64',
+            unicode:'66FE'
+        },
+        {
+            char:'兆',
+            big5:'A5FC',
+            cns:'1-473E',
+            unicode:'5146'
+        },
+        {
+            char:'禾',
+            big5:'A5DD',
+            cns:'1-467D',
+            unicode:'79BE'
+        }
+    ];
 
+    var ele = document.getElementById('div1');
+
+    for(var i=0;i<3;i++)
+    {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5= ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns= ';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += ' unicode= ';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+
+
+    }
 }
 
 /*
